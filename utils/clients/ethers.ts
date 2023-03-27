70b920746efb12ed59a6b4418314445ea37d7d90
gzeon <95478735+gzeoneth@users.noreply.github.com>
2023-03-28 01:19:43 +0800

    chore: add L1_RPC

--- utils/clients/ethers.ts
+++ utils/clients/ethers.ts
@@ -1,4 +1,5 @@
 import { providers } from 'ethers'
-import { RPC_URL } from '../constants'
+import { RPC_URL, L1_RPC_URL } from '../constants'
 
 export const provider = new providers.JsonRpcProvider(RPC_URL)
+export const l1provider = new providers.JsonRpcProvider(L1_RPC_URL)
