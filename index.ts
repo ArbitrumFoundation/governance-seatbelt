a2d4a1396ef65eb0fa85d6af4e1258ee13385d6b
fredlacs <32464905+fredlacs@users.noreply.github.com>
2023-06-06 13:30:59 -0300

    fix: handle txs with no L2ToL1 logs emitted gracefully

--- index.ts
+++ index.ts
@@ -45,6 +45,7 @@ async function simL2toL1(sr: SimulationResult, simname: string) {
   const parentId = proposal.id!
 
   const rawlog = sim.transaction.transaction_info.logs?.map((l) => l.raw)
+  if(!rawlog) return []
   const L2ToL1TxEvents = parseTypedLogs(ArbSys__factory, rawlog as any, 'L2ToL1Tx')
 
   const simresults = []
